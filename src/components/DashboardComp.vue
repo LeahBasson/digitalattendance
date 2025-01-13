c59c98a38271ea4a6145c9d0b63d7e96ff9fc76a
Intern-58 <leahbasson54@gmail.com>
2025-01-13 16:31:26 +0200

    :fire:

--- src/components/DashboardComp.vue
+++ src/components/DashboardComp.vue
@@ -1,18 +1,137 @@
 <template>
-    <div>
-      <h1>Dashboard</h1>
-      <router-link to="/lca-class-one">
-        <button>Life Choices Academy C1</button>
-      </router-link>
-      <router-link to="/lca-class-two">
+  <div class="dashboard-comp">
+ 
+     <div class="dash-content-container">
+        <div class="dash-content">
+          <div class="iframe-container">
+      <iframe
+        src="/lca-class-one?fullView=false"
+        title="Life Choices Academy Class 1"
+        frameborder="0"
+        class="iframe"
+      ></iframe>
+    </div>
+
+    <router-link to="/lca-class-one?fullView=true">
+      <button>Life Choices Academy C1</button>
+    </router-link>
+        </div>
+
+        <div class="dash-content">
+          <div class="iframe-container">
+      <iframe
+        src="/lca-class-one?fullView=false"
+        title="Life Choices Academy Class 1"
+        frameborder="0"
+        class="iframe"
+      ></iframe>
+    </div>
+    <router-link to="/lca-class-two">
         <button>Life Choices Academy C2</button>
       </router-link>
+        </div>
+
+        <div class="dash-content">
+          <div class="iframe-container">
+      <iframe
+        src="/lca-class-one?fullView=false"
+        title="Life Choices Academy Class 1"
+        frameborder="0"
+        class="iframe"
+      ></iframe>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'DashboardComp',
-  }
-  </script>
-  
+    <router-link to="/lca-class-two">
+        <button>Life Choices Academy C2</button>
+      </router-link>
+        </div>
+
+        <div class="dash-content">
+          <div class="iframe-container">
+      <iframe
+        src="/lca-class-one?fullView=false"
+        title="Life Choices Academy Class 1"
+        frameborder="0"
+        class="iframe"
+      ></iframe>
+    </div>
+    <router-link to="/lca-class-two">
+        <button>Life Choices Academy C2</button>
+      </router-link>
+        </div>
+
+        <div class="dash-content">
+          <div class="iframe-container">
+      <iframe
+        src="/lca-class-one?fullView=false"
+        title="Life Choices Academy Class 1"
+        frameborder="0"
+        class="iframe"
+      ></iframe>
+    </div>
+    <router-link to="/lca-class-two">
+        <button>Life Choices Academy C2</button>
+      </router-link>
+        </div>
+
+
+        <div class="dash-content">
+          <div class="iframe-container">
+      <iframe
+        src="/lca-class-one?fullView=false"
+        title="Life Choices Academy Class 1"
+        frameborder="0"
+        class="iframe"
+      ></iframe>
+    </div>
+    <router-link to="/lca-class-two">
+        <button>Life Choices Academy C2</button>
+      </router-link>
+        </div>
+
+     </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DashboardComp",
+};
+</script>
+
+<style>
+.button-container {
+  display: flex;
+  gap: 1rem;
+  margin-bottom: 1rem;
+}
+
+.iframe-container {
+  width: 100%;
+  height: 350px;
+  margin-top: 2rem;
+  /* border: 1px solid #ddd; */
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.iframe {
+  width: 100%;
+  height: 100%;
+}
+
+.dash-content{
+  display: flex;
+  justify-content: center;
+  width: 30%;
+  flex-direction: column;
+}
+
+.dash-content-container{
+  display: flex;
+  flex-wrap: wrap;
+  width: 80%;
+  margin: auto;
+  margin-bottom: 2rem;
+}
+</style>
--- src/views/LcaClassOne.vue
+++ src/views/LcaClassOne.vue
@@ -1,24 +1,37 @@
 <template>
-    <div class="back-icon">
+    <div>
+      <div v-if="showFullView" class="back-icon">
         <router-link to="/">
-            <i class="fi fi-br-angle-small-left"></i>
-        </router-link> 
-    </div>
-    
-    <div class="heading">
-      <h1>Life Choices Academy Class 1</h1>
-      <p>Daily Logs</p>
-    </div>
-</template>
+          <i class="fi fi-br-angle-small-left"></i>
+        </router-link>
+      </div>
   
-<script>
-export default {
-name: 'LcaClassOne',
-}
-</script>
-
-<style>
-.heading{
+      <div v-if="showFullView" class="heading">
+        <h1>Life Choices Academy Class 1</h1>
+        <p>Daily Logs</p>
+      </div>
+  
+      <!-- Always display the class-one-data section -->
+      <div class="class-one-data" id="classonedata">
+        <p>This is the class one data section.</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "LcaClassOne",
+    computed: {
+      // Check the query parameter to determine what to show
+      showFullView() {
+        return this.$route.query.fullView === "true";
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .heading {
     width: 95%;
     height: 220px;
     background-color: var(--alternative);
@@ -28,27 +41,36 @@ name: 'LcaClassOne',
     margin-top: 0.5rem;
     border-radius: 2rem;
     color: var(--secondary);
-}
-
-.heading p{
+  }
+  
+  .heading p {
     font-size: 1.3rem;
-}
-
-.back-icon { 
-    display: flex; 
-    align-items: center; 
+  }
+  
+  .back-icon {
+    display: flex;
+    align-items: center;
     padding: 1rem;
     margin-left: 1rem;
-} 
-
-.back-icon i { 
-    color: var(--guestColor); 
-    font-size: 2.5rem; 
-    cursor: pointer; 
-}
-
-.back-icon a {
+  }
+  
+  .back-icon i {
+    color: var(--guestColor);
+    font-size: 2.5rem;
+    cursor: pointer;
+  }
+  
+  .back-icon a {
     text-decoration: none;
-}
-</style>
+  }
+  
+  .class-one-data {
+    background-color: #f5f5f5;
+    padding: 1rem;
+    border-radius: 10px;
+    margin: 1rem auto;
+    width: 95%;
+    text-align: center;
+  }
+  </style>
   
